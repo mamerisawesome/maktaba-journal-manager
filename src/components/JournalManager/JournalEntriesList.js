fb862ee7beadd7d60cd035581c0af548ac9cd15f
amendoza <amendoza@stratpoint.com>
2020-06-16 23:58:12 +0800

    Handle single digit time parameters

--- src/components/JournalManager/JournalEntriesList.js
+++ src/components/JournalManager/JournalEntriesList.js
@@ -28,11 +28,19 @@ const getCatEmoji = (mood) => {
 };
 
 const presentDate = dateInput => {
-    const formatted = dateInput
-        .split("/").join("-")
-        .split(" ").join("T");
-    const d = new Date(formatted + "Z");
+    let formatted = dateInput.split("/").join("-");
+    formatted = formatted.split(" ");
+    formatted[1] = formatted[1].split(":")
+        .map(timeParam => {
+            if (timeParam.length === 1) {
+                return "0" + timeParam;
+            }
+            return timeParam;
+        })
+        .join(":");
+    formatted.join("T");
 
+    const d = new Date(formatted + "Z");
     const ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d)
     const mo = new Intl.DateTimeFormat("en", { month: "long" }).format(d)
     const da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d)
