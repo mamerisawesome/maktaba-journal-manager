66bd81e73287a309971bfcc9bfc619730ed0197b
amendoza <amendoza@stratpoint.com>
2020-06-17 00:07:01 +0800

    Code cleaned auth handler

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,7 +1,9 @@
-export function checkAuth(immediate, data, callback) {
-  window.gapi.auth.authorize({
-    'client_id': process.env.REACT_APP_API_CLIENTID,
-    'scope': process.env.REACT_APP_API_SCOPE,
-    'immediate': immediate
-  }, callback);
-}
+const checkAuth = (immediate, data, callback) => {
+    window.gapi.auth.authorize({
+        'client_id': process.env.REACT_APP_API_CLIENTID,
+        'scope': process.env.REACT_APP_API_SCOPE,
+        'immediate': immediate
+    }, callback);
+};
+
+export { checkAuth };
