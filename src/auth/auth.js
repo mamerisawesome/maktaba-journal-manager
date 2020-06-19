b9d45002e4409031deb11763b48195478d216002
amendoza <amendoza@stratpoint.com>
2020-06-19 15:57:11 +0800

    Removed unused auth

--- src/auth/auth.js
+++
@@ -1,9 +0,0 @@
-const checkAuth = (immediate, data, callback) => {
-    window.gapi.auth.authorize({
-        'client_id': process.env.REACT_APP_API_CLIENTID,
-        'scope': process.env.REACT_APP_API_SCOPE,
-        'immediate': immediate
-    }, callback);
-};
-
-export { checkAuth };
